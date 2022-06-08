461ac592aa2d81c5e49649d80f79471595a0be53
Zach Nash <afreakenturtle@gmail.com>
2022-06-07 21:25:17 -0400

    created pages and setup api call

--- fitness-tracker-frontend/src/Componenets/Results.js
+++ fitness-tracker-frontend/src/Componenets/Results.js
@@ -1,12 +1,10 @@
 import React, { useState } from 'react';
-import { styled } from "@mui/material/styles";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import Collapse from "@mui/material/Collapse";
-import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Button from '@mui/material/Button';
