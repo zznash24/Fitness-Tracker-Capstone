5ed02581353d73370ffd38b04490eb0ec3d64c42
Sarah Barnett <sarah.c.barnett3@gmail.com>
2022-06-09 21:05:09 -0400

    changed-column-name-table-routes

--- fitness-tracker-frontend/src/Componenets/WorkoutApp.js
+++ fitness-tracker-frontend/src/Componenets/WorkoutApp.js
@@ -1,26 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Results from "./Results";
-import WorkSearch from './WorkSearch';
-import SearchWork from '../Hooks/SearchWork';
-
+import WorkSearch from "./WorkSearch";
+import SearchWork from "../Hooks/SearchWork";
 
 function WorkoutApp() {
-    // const [values, searchByInputHolder, searchInputTerm, handleChangeFunc] = InputState();
-    const [result, searchTags, show, handleSubmitFunc] = SearchWork();
+  // const [values, searchByInputHolder, searchInputTerm, handleChangeFunc] = InputState();
+  const [result, searchTags, handleSubmitFunc] = SearchWork();
 
-return (
+  const handleChangeFunc = () => {
+      alert('handleChangeFunc')
+  }
+
+  const values = ['']
+
+
+  return (
     <div>
-        <WorkSearch
-              handleChange={handleChangeFunc}
-              handleSubmit={handleSubmitFunc}
-              searchByInputHolder={searchByInputHolder}
-              searchInputTerm={searchInputTerm}
-              values={values} />
-        <div>
-            
-        </div>
+      <WorkSearch
+        handleChange={handleChangeFunc}
+        handleSubmit={handleSubmitFunc}
+      //  searchByInputHolder={searchByInputHolder}
+        // searchInputTerm={searchInputTerm}
+        values={values}
+      />
+      <div></div>
     </div>
-)
+  );
 }
 
 export default WorkoutApp;
