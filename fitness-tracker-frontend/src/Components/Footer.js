07f3741464c64248f0036c3f762fa38a8b044dd6
Zach Nash <afreakenturtle@gmail.com>
2022-06-16 22:58:23 -0400

    created a save button for the workout list

--- fitness-tracker-frontend/src/Components/Footer.js
+++ fitness-tracker-frontend/src/Components/Footer.js
@@ -1,13 +1,27 @@
-import react from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+
 
 function Footer() {
-    return (
-        <Box className="Footer" >
-          <Button className='Save-Work' />
-        </Box>
-      )
+  
+  let navigate = useNavigate(); 
+  const routeChange = () =>{ 
+    let path = `/favorites`; 
+    navigate(path);
+  }
+  
+  return (
+     <div className="save-work">
+      <Container>       
+          <Button 
+          color="primary" 
+          className="px-4"
+          onClick={routeChange}>
+              Go To Your Workout!
+            </Button>
+       </Container>
+    </div>
+  );
 }
 
 export default Footer;
