6578a35e8f520a9289d16374eb8b5700efef21b7
Sarah Barnett <sarah.c.barnett3@gmail.com>
2022-06-28 08:19:44 -0400

    added info to readme

--- fitness-tracker-frontend/src/Components/WorkoutApp.js
+++ fitness-tracker-frontend/src/Components/WorkoutApp.js
@@ -1,53 +1,66 @@
-import React, { useState } from 'react';
-import WorkSearch from './WorkSearch';
-import searchTerms from '../Hooks/searchTerms';
-import SearchWork from '../Hooks/SearchWork';
+import React, { useState } from "react";
+import WorkSearch from "./WorkSearch";
+import searchTerms from "../Hooks/searchTerms";
+import SearchWork from "../Hooks/SearchWork";
 import Listresult from "./resultList";
-import PageN from './Pages';
-import Footer from './Footer';
-import '../styles/WorkoutApp.css';
+import PageN from "./Pages";
+import Footer from "./Footer";
+import "../styles/WorkoutApp.css";
 
+function WorkoutApp({ userId }) {
+  const [values, searchByInputHolder, searchInputTerm, handleChangeFunc] =
+    searchTerms();
+  const [result, searchTags, show, handleSubmitFunc] = SearchWork();
+  const [itemsPerPage] = useState(4);
+  const [page, setPage] = React.useState(1);
 
+  const indexOfLastItem = page * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  console.log(result, indexOfLastItem, indexOfFirstItem);
 
-function WorkoutApp({userId}) {
-    const [values, searchByInputHolder, searchInputTerm, handleChangeFunc] = searchTerms();
-    const [result, searchTags, show, handleSubmitFunc] = SearchWork();
-    const [itemsPerPage] = useState(8);
-    const [page, setPage] = React.useState(1);
+  const currentResults = result.slice(indexOfFirstItem, indexOfLastItem);
+  let pagination;
 
-    const indexOfLastItem = page * itemsPerPage;
-    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-    console.log(result, indexOfLastItem, indexOfFirstItem);
+  console.log("WorkoutApp", userId);
 
-    const currentResults = result.slice(indexOfFirstItem, indexOfLastItem);
-    let pagination;
+  if (!show) {
+    pagination = null;
+  } else if (result.length === 0) {
+    pagination = null;
+  } else {
+    pagination = (
+      <PageN
+        itemsPerPage={itemsPerPage}
+        totalItems={result.length}
+        page={page}
+        setPage={setPage}
+      />
+    );
+  }
 
-console.log("WorkoutApp", userId)
-    
-
-    if (!show) {
-        pagination = null;
-      } else if (result.length === 0) {
-        pagination = null;
-      } else {
-        pagination = <PageN itemsPerPage={itemsPerPage} totalItems={result.length} page={page} setPage={setPage} />
-      }
-
-      const resultList = <Listresult userId={userId} data={currentResults} searchTags={searchTags} numResults={result.length} />;
-return (
+  const resultList = (
+    <Listresult
+      userId={userId}
+      data={currentResults}
+      searchTags={searchTags}
+      numResults={result.length}
+    />
+  );
+  return (
     <div>
-        <WorkSearch
-              handleChange={handleChangeFunc}
-              handleSubmit={handleSubmitFunc}
-              searchByInputHolder={searchByInputHolder}
-              searchInputTerm={searchInputTerm}
-              values={values} />
-        <div className='cards' container spacing={3}>
-            {resultList}
-        </div>
-        <hr />
-        {pagination}
-        <Footer />
+      <WorkSearch
+        handleChange={handleChangeFunc}
+        handleSubmit={handleSubmitFunc}
+        searchByInputHolder={searchByInputHolder}
+        searchInputTerm={searchInputTerm}
+        values={values}
+      />
+      <div className="cards" container spacing={3}>
+        {resultList}
+      </div>
+      <hr />
+      {pagination}
+      <Footer />
     </div>
   );
 }
