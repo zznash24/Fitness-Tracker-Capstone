9335450eb6104109d111906862c2244a311c9a6b
Zach Nash <zznash24@aol.com>
2023-05-03 14:34:02 -0400

    fixed my package json file seeding error

--- fitness-tracker-backend/config/config.js
+++ fitness-tracker-backend/config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 
 module.exports = {
   "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
+    "username": process.env.DEV_DB_USER,
+    "password": process.env.DEV_DB_PASSWORD,
+    "database": process.env.DEV_DB_DATABASE,
+    "host": process.env.DEV_DB_HOST,
     "dialect": "postgres"
   },
   "test": {
